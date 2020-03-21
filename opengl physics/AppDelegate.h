4abe2c2e9ce375c8c3db2dae4329140ffd0123aa
Max Mueggler <maximumsomething@gmail.com>
2020-03-21 16:33:05 -0400

    Initial Commit

---
+++ opengl physics/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  opengl physics
+//
+//  Created by Max on 3/21/20.
+//  Copyright © 2020 Max. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ opengl physics/AppDelegate.m
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.m
+//  opengl physics
+//
+//  Created by Max on 3/21/20.
+//  Copyright © 2020 Max. All rights reserved.
+//
+
+#import "AppDelegate.h"
+
+@interface AppDelegate ()
+
+@property (weak) IBOutlet NSWindow *window;
+@end
+
+@implementation AppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application
+}
+
+
+- (void)applicationWillTerminate:(NSNotification *)aNotification {
+	// Insert code here to tear down your application
+}
+
+
+@end
